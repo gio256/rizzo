e675c4cec73b4ddc2f75a9242569f52f72411676
gio256 <102917377+gio256@users.noreply.github.com>
2024-06-04 10:44:11 -0600

    Add remaining methods to impl_stark_no_ctls macro

--- src/util.rs
+++ src/util.rs
@@ -27,7 +27,7 @@ macro_rules! impl_stark_no_ctls {
         ::paste::paste! {
             #[cfg(test)]
             #[derive(Clone, Copy, Default)]
-            struct [<$Stark NoCtls>]<F, const D: usize>($Stark<F, D>);
+            struct [<$Stark NoCtls>]<F, const D: usize>(pub $Stark<F, D>);
 
             #[cfg(test)]
             impl<
@@ -35,6 +35,10 @@ macro_rules! impl_stark_no_ctls {
                 + ::plonky2::field::extension::Extendable<D>,
                 const D: usize
             > ::starky::stark::Stark<F, D> for [<$Stark NoCtls>]<F, D> {
+
+                const COLUMNS: usize = <$Stark<F, D> as ::starky::stark::Stark<F, D>>::COLUMNS;
+                const PUBLIC_INPUTS: usize = <$Stark<F, D> as ::starky::stark::Stark<F, D>>::PUBLIC_INPUTS;
+
                 type EvaluationFrame<FE, P, const D2: usize>
                     = <$Stark<F, D> as ::starky::stark::Stark<F, D>>::EvaluationFrame<FE, P, D2>
                 where
@@ -55,6 +59,29 @@ macro_rules! impl_stark_no_ctls {
                     self.0.eval_packed_generic(frame, cc)
                 }
 
+                fn eval_packed_base<P: ::plonky2::field::packed::PackedField<Scalar = F>>(
+                    &self,
+                    frame: &Self::EvaluationFrame<F, P, 1>,
+                    cc: &mut ::starky::constraint_consumer::ConstraintConsumer<P>,
+                ) {
+                    self.0.eval_packed_base(frame, cc)
+                }
+
+                fn eval_ext(
+                    &self,
+                    frame: &Self::EvaluationFrame<
+                        <F as ::plonky2::field::extension::Extendable<D>>::Extension,
+                        <F as ::plonky2::field::extension::Extendable<D>>::Extension,
+                        D,
+                    >,
+                    cc: &mut ::starky::constraint_consumer::ConstraintConsumer<
+                        <F as ::plonky2::field::extension::Extendable<D>>::Extension,
+                    >,
+                ) {
+                    self.0.eval_ext(frame, cc)
+                }
+
+
                 fn eval_ext_circuit(
                     &self,
                     cb: &mut ::plonky2::plonk::circuit_builder::CircuitBuilder<F, D>,
@@ -68,8 +95,47 @@ macro_rules! impl_stark_no_ctls {
                     self.0.constraint_degree()
                 }
 
+                fn quotient_degree_factor(&self) -> usize {
+                    self.0.quotient_degree_factor()
+                }
+
+                fn num_quotient_polys(&self, cfg: &::starky::config::StarkConfig) -> usize {
+                    self.0.num_quotient_polys(cfg)
+                }
+
+                fn fri_instance(
+                    &self,
+                    zeta: <F as ::plonky2::field::extension::Extendable<D>>::Extension,
+                    g: F,
+                    num_ctl_helpers: usize,
+                    num_ctl_zs: Vec<usize>,
+                    cfg: &::starky::config::StarkConfig,
+                ) -> ::plonky2::fri::structure::FriInstanceInfo<F, D> {
+                    self.0.fri_instance(zeta, g, num_ctl_helpers, num_ctl_zs, cfg)
+                }
+
+                fn fri_instance_target(
+                    &self,
+                    cb: &mut ::plonky2::plonk::circuit_builder::CircuitBuilder<F, D>,
+                    zeta: ::plonky2::iop::ext_target::ExtensionTarget<D>,
+                    g: F,
+                    num_ctl_helper_polys: usize,
+                    num_ctl_zs: usize,
+                    cfg: &::starky::config::StarkConfig,
+                ) -> ::plonky2::fri::structure::FriInstanceInfoTarget<D> {
+                    self.0.fri_instance_target(cb, zeta, g, num_ctl_helper_polys, num_ctl_zs, cfg)
+                }
+
                 fn lookups(&self) -> Vec<::starky::lookup::Lookup<F>> {
                     self.0.lookups()
+                }
+
+                fn num_lookup_helper_columns(&self, cfg: &::starky::config::StarkConfig) -> usize {
+                    self.0.num_lookup_helper_columns(cfg)
+                }
+
+                fn uses_lookups(&self) -> bool {
+                    self.0.uses_lookups()
                 }
 
                 fn requires_ctls(&self) -> bool {
