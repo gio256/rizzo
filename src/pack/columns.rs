e8455626351dfbac797bab45ae1db649804d6ae9
gio256 <102917377+gio256@users.noreply.github.com>
2024-06-05 14:21:04 -0600

    Handle sign extension in byte packing stark

--- src/pack/columns.rs
+++ src/pack/columns.rs
@@ -3,6 +3,7 @@ use core::ops::{Deref, DerefMut, Index, IndexMut};
 
 use crate::pack::N_BYTES;
 
+const N_BITS: usize = 8;
 pub(crate) const N_PACK_COLS: usize = core::mem::size_of::<PackCols<u8>>();
 pub(crate) const PACK_COL_MAP: PackCols<usize> = make_col_map();
 
@@ -16,14 +17,27 @@ pub(crate) struct RangeCheck<T> {
     pub freq: T,
 }
 
+/// Columns for the byte packing stark.
 #[repr(C)]
 #[derive(Clone, Debug, Default)]
 pub(crate) struct PackCols<T> {
+    /// 1 if this is a write operation, 0 for a read operation.
     pub f_rw: T,
+    /// 1 if `bytes` should be interpreted as signed (lb or lh).
+    pub f_signed: T, //TODO: add to ctl_looked
+    /// 1 if `bytes` should be sign extended and the sign bit is 1.
+    pub f_sign_ext: T,
+    /// The starting virtual address of the memory operation.
     pub adr_virt: T,
+    /// The timestamp of the memory operation.
     pub time: T,
+    /// Contains a single field element set to 1 at index `length - 1`.
     pub len_idx: [T; N_BYTES],
+    /// The LE byte decomposition of the value being packed or unpacked.
     pub bytes: [T; N_BYTES],
+    /// The LE bit decomposition of the most significant byte of `bytes`.
+    pub high_bits: [T; N_BITS],
+    /// Range check columns.
     pub range_check: RangeCheck<T>,
 }
 
