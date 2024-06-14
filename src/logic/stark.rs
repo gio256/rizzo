22cd11414a0cd91a2771ac80b155817d57ab38ce
gio256 <102917377+gio256@users.noreply.github.com>
2024-06-14 12:29:15 -0600

    Add tests for bit shifts in logic stark

--- src/logic/stark.rs
+++ src/logic/stark.rs
@@ -33,6 +33,18 @@ const SHIFT_OPS: [(usize, u8); 3] = [
     (LOGIC_COL_MAP.op.f_sra, Opcode::SRA as u8),
 ];
 
+pub(crate) fn ctl_looked_logic<F: Field>() -> TableWithColumns<F> {
+    let op_comb = LOGIC_OPS.map(|(f, op)| (f, F::from_canonical_u8(op)));
+    let op = Column::linear_combination(op_comb);
+    let in0 = Column::le_bits(LOGIC_COL_MAP.in0);
+    let in1 = Column::le_bits(LOGIC_COL_MAP.in1);
+    let out = Column::single(LOGIC_COL_MAP.out);
+
+    let cols = vec![op, in0, in1, out];
+    let filter = Filter::new_simple(Column::sum(LOGIC_OPS.map(fst)));
+    TableWithColumns::new(Table::Logic as usize, cols, filter)
+}
+
 pub(crate) fn ctl_looked_shift<F: Field>() -> TableWithColumns<F> {
     let op_comb = SHIFT_OPS.map(|(f, op)| (f, F::from_canonical_u8(op)));
     let op = Column::linear_combination(op_comb);
@@ -50,16 +62,84 @@ pub(crate) fn ctl_looked_shift<F: Field>() -> TableWithColumns<F> {
     TableWithColumns::new(Table::Logic as usize, cols, filter)
 }
 
-pub(crate) fn ctl_looked_logic<F: Field>() -> TableWithColumns<F> {
-    let op_comb = LOGIC_OPS.map(|(f, op)| (f, F::from_canonical_u8(op)));
-    let op = Column::linear_combination(op_comb);
-    let in0 = Column::le_bits(LOGIC_COL_MAP.in0);
-    let in1 = Column::le_bits(LOGIC_COL_MAP.in1);
-    let out = Column::single(LOGIC_COL_MAP.out);
+/// Logical shift towards the most significant bit.
+/// [ 1 0 0 0 1 0 0 0 ] input bits (LE)
+/// [ 0 0 0 1 0 0 0 0 ] shift_amt, indexed by n
+/// [ 0 0 0 1 0 0 0 1 ] output
+///```ignore
+/// let mut sll_out = P::ZEROS;
+/// for (n, f_n) in lv.in1.into_iter().enumerate() {
+///     for m in n..WORD_BITS {
+///         sll_out += f_n * lv.in0[m - n] * P::Scalar::from_canonical_u32(1 << m);
+///     }
+/// }
+///```
+/// n is the (hypothetical) number of bits to shift by.
+/// f_n is a flag indicating whether this is really the n to shift by.
+/// All bits in the output with index < n are 0.
+/// We match up bits[..WORD_BITS - n] with the sequence (2^n, 2^n+1, ...).
+fn sll<P: PackedField>(bits: &[P; WORD_BITS], shift_amt: &[P; WORD_BITS]) -> P {
+    shift_amt
+        .iter()
+        .enumerate()
+        .flat_map(|(n, &f_n)| {
+            bits.iter()
+                .take(WORD_BITS - n)
+                .zip(P::Scalar::TWO.powers().skip(n))
+                .map(move |(&bit, base)| f_n * bit * base)
+        })
+        .sum()
+}
 
-    let cols = vec![op, in0, in1, out];
-    let filter = Filter::new_simple(Column::sum(LOGIC_OPS.map(fst)));
-    TableWithColumns::new(Table::Logic as usize, cols, filter)
+/// Logical shift towards the least significant bit.
+///[ 0 0 0 1 0 0 0 1 ] input bits (LE)
+///[ 0 0 0 1 0 0 0 0 ] shift_amt, indexed by n
+///[ 1 0 0 0 1 0 0 0 ] output
+///```ignore
+/// let mut srl_out = P::ZEROS;
+/// for (n, f_n) in lv.in1.into_iter().enumerate() {
+///     for m in n..WORD_BITS {
+///         srl_out += f_n * lv.in0[m] * P::Scalar::from_canonical_u32(1 << (m - n));
+///     }
+/// }
+///```
+/// n is the (hypothetical) number of bits to shift by.
+/// f_n is a flag indicating whether this is really the n to shift by.
+/// We match up bits[n..] with the sequence (2^0, 2^1, ...).
+fn srl<P: PackedField>(bits: &[P; WORD_BITS], shift_amt: &[P; WORD_BITS]) -> P {
+    shift_amt
+        .iter()
+        .enumerate()
+        .flat_map(|(n, &f_n)| {
+            bits.iter()
+                .skip(n)
+                .zip(P::Scalar::TWO.powers())
+                .map(move |(&bit, base)| f_n * bit * base)
+        })
+        .sum()
+}
+
+/// Aithmetic shift towards the least significant bit.
+///[ 0 0 0 1 0 0 0 1 ] input bits (LE)
+///[ 0 0 0 1 0 0 0 0 ] shift_amt, indexed by n
+///[ 1 0 0 0 1 1 1 1 ] output
+///
+/// n is the (hypothetical) number of bits to shift by.
+/// f_n is a flag indicating whether this is really the n to shift by.
+/// We repeat bits[WORD_SIZE - 1] n times, scaling by 2^(WORD_BITS - n)..2^WORD_BITS.
+fn sra_ext<P: PackedField>(bits: &[P; WORD_BITS], shift_amt: &[P; WORD_BITS]) -> P {
+    let ext_bit = *bits.last().unwrap();
+    shift_amt
+        .iter()
+        .enumerate()
+        .flat_map(|(n, &f_n)| {
+            P::Scalar::TWO
+                .powers()
+                .skip(WORD_BITS - n)
+                .take(n)
+                .map(move |base| f_n * ext_bit * base)
+        })
+        .sum()
 }
 
 fn eval_all<P: PackedField>(lv: &LogicCols<P>, nv: &LogicCols<P>, cc: &mut ConstraintConsumer<P>) {
@@ -90,75 +170,15 @@ fn eval_all<P: PackedField>(lv: &LogicCols<P>, nv: &LogicCols<P>, cc: &mut Const
         cc.constraint(bit * (bit - P::ONES));
     }
 
-    let bits = lv.in0;
-    let shift_amt = lv.in1;
+    //TODO: constraints for AND, OR, XOR
 
-    // sll: logical shift towards the most significant bit.
-    //[ 1 0 0 0 1 0 0 0 ] input bits
-    //[ 0 0 0 1 0 0 0 0 ] shift_amt, indexed by n
-    //[ 0 0 0 1 0 0 0 1 ] output
-    //
-    // let mut sll_out = P::ZEROS;
-    // for (n, f_n) in lv.in1.into_iter().enumerate() {
-    //     for m in n..WORD_BITS {
-    //         sll_out += f_n * lv.in0[m - n] * P::Scalar::from_canonical_u32(1 << m);
-    //     }
-    // }
-    //
-    // n is the (hypothetical) number of bits to shift by.
-    // f_n is a flag indicating whether this is really the n to shift by.
-    // All bits in the output with index < n are 0.
-    let sll_out: P = shift_amt
-        .into_iter()
-        .enumerate()
-        .flat_map(|(n, f_n)| {
-            bits.iter()
-                .take(WORD_BITS - n)
-                .zip(P::Scalar::TWO.powers().skip(n))
-                .map(move |(&bit, base)| f_n * bit * base)
-        })
-        .sum();
+    let sll_out = sll(&lv.in0, &lv.in1);
     cc.constraint(f_sll * (lv.out - sll_out));
 
-    // srl: logical shift towards the least significant bit
-    //[ 0 0 0 1 0 0 0 1 ] input bits
-    //[ 0 0 0 1 0 0 0 0 ] shift_amt, indexed by n
-    //[ 1 0 0 0 1 0 0 0 ] output
-    //
-    // let mut srl_out = P::ZEROS;
-    // for (n, f_n) in lv.in1.into_iter().enumerate() {
-    //     for m in n..WORD_BITS {
-    //         srl_out += f_n * lv.in0[m] * P::Scalar::from_canonical_u32(1 << (m - n));
-    //     }
-    // }
-    let srl_out: P = shift_amt
-        .into_iter()
-        .enumerate()
-        .flat_map(|(n, f_n)| {
-            bits.iter()
-                .skip(n)
-                .zip(P::Scalar::TWO.powers())
-                .map(move |(&bit, base)| f_n * bit * base)
-        })
-        .sum();
+    let srl_out = srl(&lv.in0, &lv.in1);
     cc.constraint(f_srl * (lv.out - srl_out));
 
-    // sra: aithmetic shift towards the least significant bit
-    //[ 0 0 0 1 0 0 0 1 ] input bits
-    //[ 0 0 0 1 0 0 0 0 ] shift_amt, indexed by n
-    //[ 1 0 0 0 1 1 1 1 ] output
-    let ext_bit = *bits.last().unwrap();
-    let sra_ext: P = shift_amt
-        .into_iter()
-        .enumerate()
-        .flat_map(|(n, f_n)| {
-            P::Scalar::TWO
-                .powers()
-                .skip(WORD_BITS - n)
-                .take(n)
-                .map(move |base| f_n * ext_bit * base)
-        })
-        .sum();
+    let sra_ext = sra_ext(&lv.in0, &lv.in1);
     let sra_out = srl_out + sra_ext;
     cc.constraint(f_sra * (lv.out - sra_out));
 }
@@ -226,10 +246,12 @@ impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for LogicStark<F,
 
 #[cfg(test)]
 mod tests {
+    use plonky2::field::types::{Field, PrimeField64, Sample};
     use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
+    use rand::Rng;
     use starky::stark_testing::{test_stark_circuit_constraints, test_stark_low_degree};
 
-    use super::LogicStark;
+    use super::{sll, sra_ext, srl, LogicStark, WORD_BITS};
 
     const D: usize = 2;
     type C = PoseidonGoldilocksConfig;
@@ -247,4 +269,52 @@ mod tests {
     //     let stark: S = Default::default();
     //     test_stark_circuit_constraints::<F, C, S, D>(stark).unwrap();
     // }
+
+    fn to_le_bits<F: Field>(x: u32) -> [F; WORD_BITS] {
+        core::array::from_fn(|i| F::from_bool(x & (1 << i) != 0))
+    }
+
+    #[test]
+    fn test_sll() {
+        let mut rng = rand::thread_rng();
+        let x: u32 = rng.gen();
+        let shift = rng.gen_range(0..WORD_BITS);
+        let expect = F::from_canonical_u32(x << shift);
+
+        let x_bits = to_le_bits(x);
+        let mut shift_amt = [F::ZERO; WORD_BITS];
+        shift_amt[shift] = F::ONE;
+        let out = sll(&x_bits, &shift_amt);
+        assert_eq!(out, expect);
+    }
+
+    #[test]
+    fn test_srl() {
+        let mut rng = rand::thread_rng();
+        let x: u32 = rng.gen();
+        let shift = rng.gen_range(0..WORD_BITS);
+        let expect = F::from_canonical_u32(x >> shift);
+
+        let x_bits = to_le_bits(x);
+        let mut shift_amt = [F::ZERO; WORD_BITS];
+        shift_amt[shift] = F::ONE;
+        let out = srl(&x_bits, &shift_amt);
+        assert_eq!(out, expect);
+    }
+
+    #[test]
+    fn test_sra() {
+        let mut rng = rand::thread_rng();
+        let x: i32 = rng.gen();
+        let shift = rng.gen_range(0..WORD_BITS);
+        let expect = F::from_canonical_u32((x >> shift) as u32);
+
+        let x_bits = to_le_bits(x as u32);
+        let mut shift_amt = [F::ZERO; WORD_BITS];
+        shift_amt[shift] = F::ONE;
+        let srl_out = srl(&x_bits, &shift_amt);
+        let sra_ext = sra_ext(&x_bits, &shift_amt);
+        let out = srl_out + sra_ext;
+        assert_eq!(out, expect);
+    }
 }
